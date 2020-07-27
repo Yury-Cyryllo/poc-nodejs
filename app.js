18fe7343e457b4e6cc161682d998f01bfabd23d2
Yury-Cyryllo <68504343+Yury-Cyryllo@users.noreply.github.com>
2020-07-27 19:24:47 -0300

    Meu segundo commit

--- app.js
+++ app.js
@@ -2,15 +2,7 @@ var http = require("http");
 var fs = require("fs")
 
 http.createServer(function(request,response){
-    fs.readFile("index.html", function(error, conteudo){
-        if(error){
-            console.log(error);
-        }
-        else {
-            response.write(conteudo);
-        }
-        response.end();
-    })
+    
 }).listen(8081);
 
 console.log("Servidor rodando em http://localhost:8081");
